c6356aadd27034c1a03aebb1f6dd7faae0595310
Marc Neudert <marc.neudert@gmail.com>
2016-03-27 11:55:08 +0200

    nests data internally in agent adapter

--- lib/plug/pagecache/adapter/agent.ex
+++ lib/plug/pagecache/adapter/agent.ex
@@ -8,13 +8,24 @@ defmodule Plug.PageCache.Adapter.Agent do
 
   # Adapter lifecycle
 
-  def init(_opts), do: { :ok, %{} }
+  def init(_opts), do: { :ok, %{ entries: %{} } }
 
 
   # Adapter methods
 
-  def clean(_state),           do: { %{}, :ok }
-  def load(state, path),       do: { state, Map.get(state, path, nil) }
-  def remove(state, path),     do: { Map.delete(state, path), :ok }
-  def save(state, path, page), do: { Map.put(state, path, page), :ok }
+  def clean(state) do
+    { %{ state | entries: %{} }, :ok }
+  end
+
+  def load(state, path) do
+    { state, Map.get(state.entries, path, nil) }
+  end
+
+  def remove(state, path) do
+    { %{ state | entries: Map.delete(state.entries, path) }, :ok }
+  end
+
+  def save(state, path, page) do
+    { %{ state | entries: Map.put(state.entries, path, page) }, :ok }
+  end
 end
