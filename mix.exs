d33064959b3f50844929ee1869fb60ef8b49891a
Marc Neudert <marc.neudert@gmail.com>
2016-03-27 12:24:08 +0200

    bumps version to v0.3.0-dev

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Plug.PageCache.Mixfile do
   def project do
     [ app:     :plug_pagecache,
       name:    "Plug.PageCache",
-      version: "0.2.0",
+      version: "0.3.0-dev",
       elixir:  "~> 1.2",
       deps:    deps,
 
@@ -47,7 +47,7 @@ defmodule Plug.PageCache.Mixfile do
   defp docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "readme",
-      source_ref: "v0.2.0",
+      source_ref: "master",
       source_url: @url_github ]
   end
 
